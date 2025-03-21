142476c7afbb107be127fde1e7bed45a1ca02b23
simong2 <simon.getahun@icloud.com>
2025-03-21 03:42:55 -0400

    updated connection URL to deployed one

--- app/database.py
+++ app/database.py
@@ -5,10 +5,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
-user = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_USER_PASSWORD")
+# local
+# user = os.getenv("POSTGRES_USER")
+# password = os.getenv("POSTGRES_USER_PASSWORD")
+# URL_DATABASE = f"postgresql://{user}:{password}@localhost/stt_database"
 
-URL_DATABASE = f"postgresql://{user}:{password}@localhost/stt_database"
+# render
+user = os.getenv("RENDER_USERNAME")
+password = os.getenv("RENDER_PASSWORD")
+host = os.getenv("RENDER_HOST")
+db_name = os.getenv("RENDER_NAME")
+
+URL_DATABASE = f"postgresql://{user}:{password}@{host}/{db_name}"
+
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
